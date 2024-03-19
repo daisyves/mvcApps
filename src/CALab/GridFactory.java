1fc4433dbdbd0fb41a37133354b166295fe009d6
NayyH <nayhtet0907@gmail.com>
2024-03-18 22:53:10 -0700

    GridFactory done

--- src/CALab/GridFactory.java
+++ src/CALab/GridFactory.java
@@ -1,4 +1,44 @@
 package CALab;
+import mvc.*;
+public abstract class GridFactory implements AppFactory {
+    @Override
+    public abstract Model makeModel();
 
-public class GridFactory {
+    @Override
+    public abstract View makeView(Model m);
+
+    @Override
+    public String getTitle() {
+        return "CA LAB";
+    }
+
+    @Override
+    public String[] getHelp() {
+        return new String[0];
+    }
+
+    @Override
+    public String about() {
+        return "CA Lab Program";
+    }
+
+    @Override
+    public String[] getEditCommands() {
+        return new String[]{"RUN1", "RUN50", "POPULATE", "CLEAR"};
+    }
+
+    @Override
+    public Command makeEditCommand(Model model, String type, Object source) {
+        switch (type) {
+            case "RUN1":
+                return new UpdateLoop(model, 1);
+            case "RUN50":
+                return new UpdateLoop(model, 50);
+            case "POPULATE":
+                return new Populate(model);
+            case "CLEAR":
+                return new Clear(model);
+        }
+        return null;
+    }
 }
